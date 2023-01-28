d6ec9015e63f816c88cab93361b209f1cb46fb4b
Masha <suvorova-2007@mail.ru>
2023-01-28 23:22:59 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int GetNumber(string message)
+{
+    int resultNumber = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out resultNumber))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод!");
+        }
+    }
+    return resultNumber;
+}
+
+int number = GetNumber("Введите число");
+
+int GetSumOfFigure(int number)
+{
+    int sum = 0;
+    int figure = 0;
+    while (number>=1)
+    {
+        figure = number % 10;
+        sum = sum + figure;
+        number = number / 10;
+    }   
+    return sum;
+}
+
+Console.WriteLine($"сумма цифр числа {number} равна {GetSumOfFigure(number)}");
